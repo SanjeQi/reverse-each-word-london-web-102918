b7ef82fb5cf8ebf12785004fb05881a98b5d37e1
bright-reducer-1915 <cristi.nord@gmail.com>
2018-10-18 20:39:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,14 @@
-#def reverse_each_word(sting)
- # arr = string.split(/ /)
-  #arr_rev = []
-  #arr.each do |word|
-   # arr_rev.push(word.reverse)
-  #end
-  #return arr_rev.join(",")
-#end
+def reverse_each_word(sting)
+  arr = string.split(" ")
+  arr_rev = []
+  arr.each do |word|
+    arr_rev.push(word.reverse)
+  end
+  return arr_rev.join(",")
+end
 
-def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
-  word.reverse
-end
-  reversed.join(" ")
-end
+#def reverse_each_word(sentence)
+ # reversed = sentence.split(" ").collect do |word|
+  #word.reverse
+## reversed.join(" ")
+#end
